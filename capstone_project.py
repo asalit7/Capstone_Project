4dd8fe6ee1d958976bf9e94b5de837a337057c5a
AlecS <alec.salit@gmail.com>
2022-05-17 12:03:33 -0400

    Making first commits

--- capstone_project.py
+++ capstone_project.py
@@ -7,4 +7,4 @@ f1 = pd.read_csv("accepted_2007_to_2018Q4.csv.gz", compression='gzip')
 f2 = pd.read_csv("rejected_2007_to_2018Q4.csv.gz", compression='gzip')
 acc_df = f1.sample(n = 100000)
 rej_df = f2.sample(n = 100000)
-
+print(acc_df.head())
