5608b331f7416f08a6b90f57f4815ca2bef8924f
AlecS <alec.salit@gmail.com>
2022-05-17 11:45:53 -0400

    first commit

---
+++ capstone_project.py
@@ -0,0 +1,10 @@
+#csv test
+import numpy as np
+import pandas as pd 
+
+
+f1 = pd.read_csv("accepted_2007_to_2018Q4.csv.gz", compression='gzip')
+f2 = pd.read_csv("rejected_2007_to_2018Q4.csv.gz", compression='gzip')
+acc_df = f1.sample(n = 100000)
+rej_df = f2.sample(n = 100000)
+
