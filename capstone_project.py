9624b2ebdb81c7c6c1d4dbf2ba577bb8ba932d82
AlecS <alec.salit@gmail.com>
2022-05-17 12:26:24 -0400

    looking at dataset

--- capstone_project.py
+++ capstone_project.py
@@ -7,4 +7,7 @@ f1 = pd.read_csv("accepted_2007_to_2018Q4.csv.gz", compression='gzip')
 f2 = pd.read_csv("rejected_2007_to_2018Q4.csv.gz", compression='gzip')
 acc_df = f1.sample(n = 100000)
 rej_df = f2.sample(n = 100000)
-print(acc_df.head())
+print(acc_df.head().T)
+acc_df[acc_df['grade'] == 'A'].describe().T
+default_col = ['Does not meet the credit policy. Status:Charged Off', ' Charged Off', 'Default']
+acc_df[acc_df['loan_status'].isin(default_col)].describe().T
