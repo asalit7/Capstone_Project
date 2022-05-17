01cba7c87a651144e4e864a605cbd6500fed2df8
AlecS <alec.salit@gmail.com>
2022-05-17 15:49:07 -0400

    dropping values with high na values

--- capstone_project.py
+++ capstone_project.py
@@ -1,7 +1,7 @@
 #csv test
 import numpy as np
 import pandas as pd 
-
+import seaborn as sns
 
 f1 = pd.read_csv("accepted_2007_to_2018Q4.csv.gz", compression='gzip')
 f2 = pd.read_csv("rejected_2007_to_2018Q4.csv.gz", compression='gzip')
@@ -11,3 +11,18 @@ print(acc_df.head().T)
 acc_df[acc_df['grade'] == 'A'].describe().T
 default_col = ['Does not meet the credit policy. Status:Charged Off', ' Charged Off', 'Default']
 acc_df[acc_df['loan_status'].isin(default_col)].describe().T
+
+rej_df[rej_df['grade'] == 'A'].describe().T
+default_col = ['Does not meet the credit policy. Status:Charged Off', ' Charged Off', 'Default']
+rej_df[rej_df['loan_status'].isin(default_col)].describe().T
+
+nan_check1 = acc_df.isna().mean()
+nan_check1 = nan_check1[nan_check1 != 0].sort_values()
+nan_check1
+
+nan_check2 = rej_df.isna().mean()
+nan_check2 = nan_check2[nan_check2 > .9].sort_values()
+nan_check2
+
+acc_df = acc_df.drop(['desc','member_id'], axis=1, errors='ignore')
+acc_df = acc_df.drop(['desc','member_id'], axis=1, errors='ignore')
